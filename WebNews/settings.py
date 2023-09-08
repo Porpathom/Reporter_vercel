88557ca54f2fc78964de30b3326eac9b8b585813
Porpathom <117138205+Porpathom@users.noreply.github.com>
2023-09-09 03:12:22 +0700

    1

--- WebNews/settings.py
+++ WebNews/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'statics',
+)
 
-STATICFILES_DIRS = [BASE_DIR / 'statics', ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'statics')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
