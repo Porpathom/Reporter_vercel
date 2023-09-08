17c2186595a95c39539686ba9581ae6b0e6a8f3a
Porpathom <117138205+Porpathom@users.noreply.github.com>
2023-09-09 03:39:21 +0700

    1

--- WebNews/settings.py
+++ WebNews/settings.py
@@ -81,12 +81,12 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'postgres',
-        # 'USER' :'postgres',
-        # 'PASSWORD' :'pathom0966788627',
-        # 'HOST' : 'db.rvuwltkzoixmmcsdnfwv.supabase.co',
-        # 'PORT' : '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER' :'postgres',
+        'PASSWORD' :'pathom0966788627',
+        'HOST' : 'db.rvuwltkzoixmmcsdnfwv.supabase.co',
+        'PORT' : '5432'
     }
 }
 
