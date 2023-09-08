42a56f1e3bc1f8e28c6fae24e80eb2681ef0e058
Porpathom <117138205+Porpathom@users.noreply.github.com>
2023-09-09 03:37:02 +0700

    1

--- WebNews/settings.py
+++ WebNews/settings.py
@@ -127,13 +127,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'statics',
-)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'statics')
+STATICFILES_DIRS = [BASE_DIR / 'statics', ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
